a9f9f9f7aa0dca7649d1d25bb3b0a4f56467040a
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-15 01:53:19 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -17,7 +17,7 @@ while True:
         nummf = file.read().splitlines()
 
     nums = []
-    nuumf(str)
+    nummf(str)
     compnum = numero + nummf
     numero_parsed = phonenumbers.parse(compnum)
     operadora = carrier.name_for_number(numero_parsed, "pt-br")

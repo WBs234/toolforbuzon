4b95f8c3bcc6aec0fe2423b5c8b5d9f209210f45
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-16 23:29:10 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,4 @@
+GNU nano 7.2                                                             b2.py
 import phonenumbers
 import time
 import os
@@ -5,34 +6,134 @@ from phonenumbers import carrier
 
 verde = "\033[92m"
 vermelho = "\033[91m"
+azul = '\033[94m'
+ciano = '\033[96m'
+magenta = '\033[95m'
+marrom = '\033[33m'
+branco = '\033[97m'
+
+nums = []
 
 while True:
     os.system("clear")
     time.sleep(1)
-    numero = input("Digite a base (+55XXXXXX): ")
+    print("\n")
+    print(ciano+"┏"+"━━━━"*5+"┓")
+    print(ciano+" ["+marrom+"1"+ciano+"] "+marrom+"BruteBuzon")
+    print(ciano+" ["+marrom+"2"+ciano+"] "+marrom+"Verificar Número")
+    print("     Específico")
+    print(ciano+"┗"+"━━━━"*5+"┛")
+    print("\n")
+    print(branco+"Oque deseja? ")
+    fa=input(ciano+"["+marrom+"•"+ciano+"] "+marrom)
+    print("\n")
+    if fa=="1":
+        numero = input(branco+"Digite a base (+55XXXXXXX): "+marrom)
+        nuf = "f.txt"
+        with open(nuf, "r") as file:
+            for palavra in file:
+                palavra=palavra.rstrip()
+                numero=str(numero)+palavra
+            def validar_numero_telefone(numero):
+                try:
+                    numero_parseado = phonenumbers.parse(numero, "BR")
+            if phonenumbers.is_valid_number(numero_parseado):
+                return True
+            else:
+                return False
+            except phonenumbers.phonenumberutil.NumberParseException:
+                return False
+        if validar_numero_telefone(numero):
+            numero_parseado = phonenumbers.parse(numero, "BR")
+            operadora = carrier.name_for_number(numero_parseado, "pt-br")
+            os.system("clear")
 
-    nuf = "f.txt"
-    
-    with open(nuf, "r") as file:
-        nummf = file.readlines()
-        nummf = str(nummf)
-        while nummf != "":
-            print(nummf.strip())
-                
+            if operadora == "Claro":
+                print(numero + verde + " é Claro!")
+                nums.append(numero)
+            else:
+                print(numero + vermelho + " não é Claro!")
 
-
-    nums = []
-    
-    compnum = str(numero) + nummf
-    numero_parsed = phonenumbers.parse(compnum)
-    operadora = carrier.name_for_number(numero_parsed, "pt-br")
-               
-    if operadora == "Claro":
-         print(numero + verde + " é Claro!")
-         nums.append(nums)
-    else:
-         print(numero + vermelho + " não é Claro!")
-
-    os.system("clear")
-    print("Os números válidos são:", nums)
-
+        os.system("clear")
+        print("Os números válidos são:")
+        print(nums)
+        print("\n")
+        print("Deseja tentar de novo?"+marrom+"[S|N]"+vermelho+" (Não esqueça de salvar os números)")
+        re=input(ciano+"["+marrom+"•"+ciano+"] "+marrom)
+        if re=="S":
+            os.system("clear")
+        else:
+            break
+    elif fa== "2":
+        print(branco+"Digite o número assim "+marrom+"(+558877776666):")
+        numero=input(ciano+"["+marrom+"•"+ciano+"] "+marrom)
+        def validar_numero_telefone(numero):
+            try:
+                numero_parseado = phonenumbers.parse(numero, "BR")
+                if phonenumbers.is_valid_number(numero_parseado):
+                    return True
+                else:
+                    return False
+            except phonenumbers.phonenumberutil.NumberParseException:
+                return False
+        def operadora_3():
+            if operadora_nome== "Claro":
+                print(vermelho)
+                print(" ______________ ")
+                print(" __  ____/__  /_____ _____________ ")
+                print(" _  /    __  /_  __ `/_  ___/  __ |")
+                print(" / /___  _  / / /_/ /_  /   / /_/ /")
+                print(" \____/  /_/  \__,_/ /_/    \____/ ")
+            elif operadora_nome == "Vivo":
+                print(ciano)
+                print(" ___    ______              ")
+                print(" __ |  / /__(_)__   _______ ")
+                print(" __ | / /__  /__ | / /  __ /")
+                print(" __ |/ / _  / __ |/ // /_/ /")
+                print(" _____/  /_/  _____/ \____/")
+            elif operadora_nome== "Oi":
+                print(verde)
+                print(" ____________ ")
+                print(" __  __ \__(_)")
+                print(" _  / / /_  / ")
+                print(" / /_/ /_  /  ")
+                print(" \____/ /_/   ")
+            elif operadora_nome== "Tim":
+                print(Azul)
+                print(" ____________            ")
+                print(" ___  __/__(_)______ ___ ")
+                print(" __  /  __  /__  __ `__ |")
+                print(" _  /   _  / _  / / / / /")
+                print(" /_/    /_/  /_/ /_/ /_/ ")
+        if validar_numero_telefone(numero):
+            numero_parseado = phonenumbers.parse(numero, "BR")
+            operadora_nome = carrier.name_for_number(numero_parseado, "pt-br")
+            os.system("clear")
+            print(ciano+"┏"+"━━━━"*5+"┓")
+            print(" "+marrom+numero)
+            print("\n")
+            print(verde+" Válido")
+            operadora_3()
+            print(ciano+"┗"+"━━━━"*5+"┛")
+            time.sleep(2)
+            print("\n")
+            print("Deseja tentar de novo?"+marrom+"[S|N]"+marrom)
+            re=input(ciano+"["+marrom+"•"+ciano+"] "+marrom)
+            if re=="S":
+                os.system("clear")
+            else:
+                break
+        else:
+            print(ciano+"┏"+"━━━━"*5+"┓")
+            print(marrom+numero)
+            print("\n")
+            print(vermelho+"INVALIDO")
+            print(ciano+"┗"+"━━━━"*5+"┛")
+            time.sleep(2)
+            print("\n")
+            print("Deseja tentar de novo?"+marrom+"[S|N]"+marrom)
+            re=input(ciano+"["+marrom+"•"+ciano+"] "+marrom)
+            if re=="S":
+                os.system("clear")
+            else:
+                break

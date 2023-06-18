9997d973674d98769cee2df06419ef5aaeabc328
Wolf404 <96954406+WBs234@users.noreply.github.com>
2023-06-18 20:07:02 -0300

    Update executeme.py

--- executeme.py
+++ executeme.py
@@ -1,11 +1,14 @@
 import os
-print("INSTALADOR")
-print("\n")
-os.system("pip install --upgrade pip")
 os.system("clear")
 print("INSTALADOR")
 print("\n")
+os.system("pip install --upgrade pip")
 os.system("pip install phonenumbers")
+os.system("pip install twilio")
+os.system("clear")
+print("INSTALAÇÃO FINALIZADA")
+print("Criando wordlist..")
+time.sleep(2)
 with open('f.txt', 'w') as file:
     for i in range(10000):
         number = str(i).zfill(4)
